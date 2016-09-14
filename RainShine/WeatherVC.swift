eb2c126e3d386525b745838e16a203f0ea2a7940
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-13 23:20:39 -0500

    initial

---
+++ RainShine/WeatherVC.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherVC.swift
+//  RainShine
+//
+//  Created by D on 9/13/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+   
+
+}
+
