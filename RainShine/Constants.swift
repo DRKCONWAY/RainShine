ed30315846a7e5b5187f2edb6617f878cc98faab
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-14 18:54:03 -0500

    major changes

---
+++ RainShine/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  RainShine
+//
+//  Created by D on 9/14/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "1061dd97af6a279dd6226168ea0acf63"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITITUDE)33.9\(LONGITUDE)-117.3\(APP_ID)\(API_KEY)"
+
---
+++ RainShine/CurrentWeather.swift
@@ -0,0 +1,78 @@
+//
+//  CurrentWeather.swift
+//  RainShine
+//
+//  Created by D on 9/14/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CurrentWeather {
+    // data hiding (best practice) to protect my data
+    
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: Double!
+    
+    var cityName: String {
+        if _cityName == nil{
+            _cityName = ""
+        }
+        return _cityName
+    }
+    var date: String {
+        if _date == nil{
+            _date = ""
+        }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today, \(currentDate)"
+        
+        return _date
+    }
+    var weatherType: String {
+        if _weatherType == nil{
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    var currentTemp: Double {
+        if _currentTemp == nil{
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
