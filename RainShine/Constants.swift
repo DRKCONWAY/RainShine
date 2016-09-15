efcdcf5c1733a8260da5544cf49c00bf196a4c31
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-14 23:05:31 -0500

    updated the UI with data from the API

--- RainShine/Constants.swift
+++ RainShine/Constants.swift
@@ -14,7 +14,7 @@ let LONGITUDE = "&lon="
 let APP_ID = "&appid="
 let API_KEY = "1061dd97af6a279dd6226168ea0acf63"
 
-let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITITUDE)33.9\(LONGITUDE)-117.3\(APP_ID)\(API_KEY)"
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITITUDE)32.51\(LONGITUDE)-93.73\(APP_ID)\(API_KEY)"
 
 // this tells the function when the data is done downloading
 typealias DownloadComplete = () -> ()
--- RainShine/CurrentWeather.swift
+++ RainShine/CurrentWeather.swift
@@ -48,35 +48,42 @@ class CurrentWeather {
         return _currentTemp
     }
 
-    func downloadWeatherDetails(completed: DownloadComplete) {
+    func downloadWeatherDetails(completed: @escaping DownloadComplete) {
         // Alamofire download
         let currentWeatherURL = URL(string: CURRENT_WEATHER_URL)!
         Alamofire.request(currentWeatherURL, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseJSON { (response) in
             
             let result = response.result
-            print(response)
-       }
-        
-        completed()
-  }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                    print(self._cityName)
+                }
+                if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
+                    
+                    if let main = weather[0]["main"] as? String {
+                        self._weatherType = main.capitalized
+                        print(self._weatherType)
+                    }
+                }
+                
+                if let main = dict["main"] as? Dictionary<String, AnyObject> {
+                    
+                    if let currentTemperature = main["temp"] as? Double {
+                        //conversion to farenheit
+                        let kelvinToFarenheitPreDivision = (currentTemperature * (9/5) - 459.67)
+                        let kelvinToFarenheit = Double(round(10 * kelvinToFarenheitPreDivision/10))
+                        self._currentTemp = kelvinToFarenheit
+                        print(self._currentTemp)
+                    }
+                }
+            completed()
+        }
+    }
+}
+   
     
     
     
@@ -86,3 +93,29 @@ class CurrentWeather {
     
     
 }
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
